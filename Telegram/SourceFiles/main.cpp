e95fe75eb94e8d1a84b31ad356e50ae3729aadf7
ramonrosalba22 <81506392+ramonrosalba22@users.noreply.github.com>
2021-09-28 14:32:28 +0800

    Update main.cpp

--- Telegram/SourceFiles/main.cpp
+++ Telegram/SourceFiles/main.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "core/launcher.h"
 
+
 int main(int argc, char *argv[]) {
 	const auto launcher = Core::Launcher::Create(argc, argv);
 	return launcher ? launcher->exec() : 1;
